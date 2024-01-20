488859b4e577714bb2550582dc4f167e1545379e
Jonathan Perry <jpper@umich.edu>
2024-01-19 22:48:44 -0600

    Finish implementing table of contents component

--- src/components/Blog/BlogDetails.js
+++ src/components/Blog/BlogDetails.js
@@ -5,8 +5,7 @@ import React from "react";
 const BlogDetails = ({ blog, slug }) => {
   return (
     <div
-      className="px-10 bg-accent text-light py-2 flex items-center justify-around flex-wrap text-xl font-medium\
-                mx-10 rounded-lg"
+      className="px-2 md:px-10 bg-accent dark:bg-accentDark text-light dark:text-dark py-2 flex items-center justify-around flex-wrap text-lg sm:text-xl font-medium mx-5 md:mx-10 rounded-lg"
     >
       <time className="m-3">
         {format(parseISO(blog.publishedAt), "LLLL d, yyyy")}
