829aaead0b9e7450b2450cb006915d1b7ed08ec9
Jonathan Perry <jpper@umich.edu>
2024-01-19 20:41:43 -0600

    Implement footer component

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import { cx } from "@/src/utils";
 import { Inter, Manrope } from "next/font/google";
 import Header from "@/src/components/Header";
+import Footer from "../components/Footer/footer";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -24,14 +25,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={cx(
-          inter.variable,
-          manrope.variable,
-          "font-mr bg-light "
-        )}
+        className={cx(inter.variable, manrope.variable, "font-mr bg-light ")}
       >
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
