5b2ad121abc28687eabf2307c21c07f5ec3607dd
Jonathan Perry <jpper@umich.edu>
2024-01-20 14:02:23 -0600

    Implemented contact page

---
+++ src/components/Contact/LottieAnimation.js
@@ -0,0 +1,15 @@
+"use client";
+import { DotLottiePlayer } from "@dotlottie/react-player";
+import React from "react";
+
+const LottieAnimation = () => {
+  return (
+    <DotLottiePlayer
+      src="/animation_llqd7ey4.lottie"
+      autoplay
+      loop
+    ></DotLottiePlayer>
+  );
+};
+
+export default LottieAnimation;
